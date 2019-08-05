b1558433b0653177eb8ed2a6b2fd8a899491396a
Lucas JAHIER <lucas@jahier.dev>
2019-08-05 03:55:10 +0200

    Initial Commit

---
+++ snakes_and_ladders/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  snakes_and_ladders
+//
+//  Created by Lucas JAHIER on 05/08/2019.
+//  Copyright © 2019 Lucas JAHIER. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
